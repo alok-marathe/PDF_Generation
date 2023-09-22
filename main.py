b2578e5fa398d14872274ffa886b4772e1c37be4
alok0710 <marathealok0710@gmail.com>
2023-09-22 20:37:00 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+
+    pdf.add_page()
+
+    # Set the header
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    for y in range(20, 290, 10):
+        pdf.line(10, y, 200, y)
+    pdf.line(10, 21, 200, 21)
+
+    # pdf.line(50, 17, 200, 17)
+    pdf.ln(263)
+    # Set the footer
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(0, 10, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+        pdf.ln(275)
+
+        # Set the footer
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(0, 10, txt=row["Topic"], align="R")
+        for y in range(20, 290, 10):
+            pdf.line(10, y, 200, y)
+        pdf.line(10, 21, 200, 21)
+
+pdf.output("output.pdf")
